35333fc0275c24b093abd0f3833cd61826615a93
rtaukir26 <tauqueer.ansari@bluebinaries.com>
2025-01-24 14:43:07 +0530

    test added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,5 +13,8 @@ router.post("/register", register);
 router.post("/login", userLogIn);
 router.get("/profile", isAuthorized, getUserInfo);
 router.get("/logout", isAuthorized, userLogout);
+router.get("/test", (req, res, next) => {
+  res.status(200).json({ name: "test", success: true });
+});
 
 module.exports = router;
